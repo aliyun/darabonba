5985ca4252b55642d9db647a275af0a55d9519a5
Gavin Zhao <git@gzgz.dev>
2022-09-18 14:34:12 -0400

    Migrate lib/env.js to TypeScript
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

---
+++ lib/env.ts
@@ -0,0 +1,43 @@
+'use strict';
+
+export class Env {
+	preEnv: Env;
+	map: Map<string, string>;
+
+	constructor(preEnv: Env) {
+		this.preEnv = preEnv;
+		this.map = new Map();
+	}
+
+	set(key: string, value: string) {
+		this.map.set(key, value);
+	}
+
+	has(key: string) {
+		return this.map.has(key);
+	}
+
+	hasDefined(key: string) {
+		if (this.map.has(key)) {
+			return true;
+		}
+
+		if (this.preEnv) {
+			return this.preEnv.hasDefined(key);
+		}
+
+		return false;
+	}
+
+	get(key: string) {
+		if (this.map.has(key)) {
+			return this.map.get(key);
+		}
+
+		if (this.preEnv) {
+			return this.preEnv.get(key);
+		}
+
+		return null;
+	}
+}
